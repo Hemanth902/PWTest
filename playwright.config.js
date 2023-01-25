debce81c6cacd374fad87dd9f43e5d6c58f00d2b
Hemanth902 <65942977+Hemanth902@users.noreply.github.com>
2023-01-25 18:25:31 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -15,39 +15,3 @@ module.exports = defineConfig({
     },
   }
 });
-const REPO = 'PWTest';
-const USER = 'Hemanth902';
-
-test('should create a bug report', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Bug] report 1',
-      body: 'Bug description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
-
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Bug] report 1',
-    body: 'Bug description'
-  }));
-});
-
-test('should create a feature request', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Feature] request 1',
-      body: 'Feature description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
-
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Feature] request 1',
-    body: 'Feature description'
-  }));
-});
