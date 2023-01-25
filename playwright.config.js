e8764df6e13df63d0148a304f164d336b15f83da
Hemanth902 <65942977+Hemanth902@users.noreply.github.com>
2023-01-25 18:29:45 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,14 @@
-// playwright.config.js
 // @ts-check
-const { defineConfig } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-module.exports = defineConfig({
-  use: {
+const REPO = 'PWTest';
+const USER = 'Hemanth902';
+
+// Request context is reused by all tests in the file.
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
     // All requests we send go to this API endpoint.
     baseURL: 'https://api.github.com',
     extraHTTPHeaders: {
@@ -13,5 +18,25 @@ module.exports = defineConfig({
       // Assuming personal access token available in the environment.
       'Authorization': `token ${process.env.API_TOKEN}`,
     },
-  }
+  });
+})
+
+test.afterAll(async ({ }) => {
+  // Dispose all responses.
+  await apiContext.dispose();
+});
+
+test('last created issue should be on the server', async ({ page, request }) => {
+  await page.goto(`https://github.com/${USER}/${REPO}/issues`);
+  await page.getByText('New Issue').click();
+  await page.getByRole('textbox', { name: 'Title' }).fill('Bug report 1');
+  await page.getByRole('textbox', { name: 'Comment body' }).fill('Bug description');
+  await page.getByText('Submit new issue').click();
+  const issueId = page.url().substr(page.url().lastIndexOf('/'));
+
+  const newIssue = await request.get(`https://api.github.com/repos/${USER}/${REPO}/issues/${issueId}`);
+  expect(newIssue.ok()).toBeTruthy();
+  expect(newIssue).toEqual(expect.objectContaining({
+    title: 'Bug report 1'
+  }));
 });
